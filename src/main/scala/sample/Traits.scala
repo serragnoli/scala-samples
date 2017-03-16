d495e1b89e4bfb38fa536cd6678232487a256cb9
Fabio Serragnoli <fabio.serragnoli@sky.uk>
2017-03-16 22:36:36 +0000

    More on Traits

--- src/main/scala/sample/Traits.scala
+++ src/main/scala/sample/Traits.scala
@@ -12,10 +12,19 @@ trait MyGreeter {
   def greet(name: String): Unit = println(s"Hello $name. How are you today?")
 }
 
+trait Similarity {
+  def isSimiliar(x: Any): Boolean
+
+  def isNotSimilar(x: Any): Boolean = !isSimiliar(x)
+}
+
 class DefaultGreeter extends MyGreeter
 
 class CustomisableGreeter(prefix: String, postfix: String) extends MyGreeter {
   override def greet(name: String): Unit = {
     println(s"$prefix $name$postfix")
   }
+}
+
+class MyPoint(x: Int, y: Int) extends Similarity {
 }
