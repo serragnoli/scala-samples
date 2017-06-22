eb2680987389440d987cff76d43fb4aa1eae7ce2
Fabio Serragnoli <serragnoli@gmail.com>
2017-06-22 18:50:06 +0100

    Added sample of Higher-Order function

---
+++ src/main/scala/sample/FunctionPassingAsArguments.scala
@@ -0,0 +1,18 @@
+package sample
+
+class FunctionPassingAsArguments {
+
+  val sayGoodMorning = (name: String) => s"Good Morning $name"
+
+  val sayGoodAfternoon = (name: String) => s"Good Afternoon $name"
+
+  def saySomething(whatToSay: String => String, name: String) = println(whatToSay(name))
+}
+
+object FunctionPassingAsArgumentsRunner {
+  def main(args: Array[String]): Unit = {
+    val myGreeter = new FunctionPassingAsArguments()
+
+    myGreeter.saySomething(myGreeter.sayGoodMorning, "Fabio")
+  }
+}
